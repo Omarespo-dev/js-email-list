d9f9999aeea6232fe64b344a18b24697398c03e6
Omarespo-dev <omaresposito21@gmail.com>
2025-01-22 16:50:22 +0100

    finish all add bonus

--- js/script.js
+++ js/script.js
@@ -8,13 +8,10 @@ const outputLi = document.getElementById("containerLista")
 // console.log(outputLi)
 
 
-// Aggiungo libreria Axios dove poi inserisco la mia api elencata sopra
-
-let arrayEmpty = []
-console.log(arrayEmpty)
-
+// FACCIO CICLO FOR DOVE GLI DICO DI STAMPARE 10 EMAIL DIVERSE
 for(let i = 0; i < 10; i++){
     
+    // USO AXIOS .GET DOVE GLI DICO CHE VADO A PRENDERE DALL API LE EMAIL
     axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
     .then (response => {
         
@@ -23,27 +20,16 @@ for(let i = 0; i < 10; i++){
         // variabile dell oggetto response.data
         const result = response.data
 
-        // Variable dove ricavo email dal data
+        // Variable dove ricavo email da result
         let email = result.response
-        
-        // Faccio push nell array
-        arrayEmpty.push(email)
-        
+        console.log(email)
 
         // STRINGA CON BACKTICK DOVE INSERISCO LA EMAIL
-        let stringa= `
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>
-        <li>${email}</li>`
+        let stringa= `<li>${email}</li>`
 
 
         // ELEMENTO DI OUTPUT
-        outputLi.innerHTML = stringa
+        outputLi.innerHTML += stringa
     })
     .catch(error => {
         
